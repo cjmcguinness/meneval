95dc31b2f98ee0db48c866c6dbc7b63e5f14513e
cjmcguinness <conor.j.mcguinness@outlook.com>
2024-09-20 15:52:27 +0100

    Update Auth.js

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -38,7 +38,7 @@ function Auth() {
     }
 
     try {
-      const response = await fetch(`http://localhost:8000/${endpoint}`, {
+      const response = await fetch(`http://localhost:8000/api/${endpoint}`, {
             method: 'POST',
             headers: { 'Content-Type':'application/json' },
             body: JSON.stringify({ email, password})
